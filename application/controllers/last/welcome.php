77b102fbe7878dc075bafdec0854d3936675cae7
thunduaga <chriswumbojumbo@msn.com>
2015-01-22 07:50:24 -0800

    why did i have to make a "last" folder..... 7:50am Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

---
+++ application/controllers/last/welcome.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of last
+ *
+ * @author Chris
+ */
+class Welcome extends Application{
+    //put your code here
+    function __construct() {
+        parent::__construct();
+    }
+    //OK
+    //I THINK I MIGHT KNOW WHATS GOING ON HERE AND I DONT LIKE IT
+    //WHY DID I HAVE TO PUT THIS IN A SEPERATE FOLDER 'last'
+    function index() {
+        $this->data['pagebody'] = 'homepage';    
+        $this->data = array_merge($this->data,$this->quotes->last());
+        $this->render();
+    }
+}
+
